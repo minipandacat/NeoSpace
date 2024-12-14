2a7799236706af167e70df3f0e4e0af6f0fd363d
Joan Elizabeth <ninjanerd101@gmail.com>
2024-12-14 12:38:17 -0600

    Buttons
    updated class names
    updated id names
    resolved button issue...for now

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
-let viewResumeSection = document.getElementById("resume");
-let interestsSection = document.getElementById("interests");
-let infiniteSection = document.getElementById("infinite");
-let inspoSection = document.getElementById("inspo");
-let resourcesSection = document.getElementById("resources");
+let viewTheResumes = document.querySelectorAll(".viewTheResume");
+let resumeCentral = document.querySelectorAll(".centralResume");
+let interestsSection = document.querySelectorAll(".interests");
+let infiniteSection = document.querySelectorAll(".infinite");
+let inspoSection = document.querySelectorAll(".inspo");
+let resourcesSection = document.querySelectorAll(".resources");
 
-function TestsFunction() {
-    var T = document.getElementById("TestsDiv")
-        displayValue = "";
-    if (T.style.display == "")
-        displayValue = "none";
-
-    T.style.display = displayValue;
+//hide all sections unless selected
+function hideAllSections() {
+    [viewTheResumes, resumeCentral, interestsSection, infiniteSection, inspoSection, resourcesSection].forEach(section => {
+        section.forEach(element => {
+            element.style.display = "none";
+        });
+    });
 }
 
+//select desired section
+function viewResumeBTN() {
+    hideAllSections();
+    viewTheResumes.forEach(element => {
+        element.style.display = "";
+    });
+}
+
+function resumeBtn() {
+    hideAllSections();
+    resumeCentral.forEach(element => {
+        element.style.display = "";
+    });
+}
+
+function interestsBtn() {
+    hideAllSections();
+    interestsSection.forEach(element => {
+        element.style.display = "";
+    });
+}
+
+function infChaptBtn() {
+    hideAllSections();
+    infiniteSection.forEach(element => {
+        element.style.display = "";
+    });
+}
+
+function inspoBtn() {
+    hideAllSections();
+    inspoSection.forEach(element => {
+        element.style.display = "";
+    });
+}
+
+function resourcesBtn() {
+    hideAllSections();
+    resourcesSection.forEach(element => {
+        element.style.display = "";
+    });
+}
