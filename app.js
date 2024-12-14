822b513ef3897a099a9262bca15198001300416d
Joan Elizabeth <ninjanerd101@gmail.com>
2024-12-14 03:05:23 -0600

    Button Work
    I hate buttons & padding
    *definitely be in issue in the future ~ future me problems*

---
+++ app.js
@@ -0,0 +1,15 @@
+let viewResumeSection = document.getElementById("resume");
+let interestsSection = document.getElementById("interests");
+let infiniteSection = document.getElementById("infinite");
+let inspoSection = document.getElementById("inspo");
+let resourcesSection = document.getElementById("resources");
+
+function TestsFunction() {
+    var T = document.getElementById("TestsDiv")
+        displayValue = "";
+    if (T.style.display == "")
+        displayValue = "none";
+
+    T.style.display = displayValue;
+}
+
